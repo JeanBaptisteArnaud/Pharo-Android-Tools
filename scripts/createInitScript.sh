3dcccd8fe7729c52b904892e94d5d3409dd5617d
jeanbaptistearnaud <jbaptiste.arnaud@gmail.com>
2014-12-12 14:19:55 +0100

    small change

--- scripts/createInitScript.sh
+++ scripts/createInitScript.sh
@@ -4,3 +4,4 @@ echo "#!/bin/sh
 export PATH=\$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
 export ANDROID_HOME=$ANDROID_HOME
 export ANDROID_NDK=$ANDROID_NDK" >> EnvironmentVariable.sh
+chmod +x EnvironmentVariable.sh
